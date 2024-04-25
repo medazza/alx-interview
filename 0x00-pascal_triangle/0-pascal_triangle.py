bd4752baa692c0dd60ef3ddedc6cc1a5c297e390
medazza <midazza27@gmail.com>
2024-04-25 19:42:38 +0100

    fix pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,20 +6,20 @@ def pascal_triangle(n):
     """ Pascal triangle"""
     if n <= 0:
         return []
-    
+
     triangle = []
     for row in range(n):
         # Create a new row
         current_row = []
-        
+
         for col in range(row + 1):
             # Calculate the value for each element in the row
             if col == 0 or col == row:
-                current_row.append(1)  # The first and last elements in each row are always 1
+                current_row.append(1)
             else:
-                # The value of an element is the sum of the two elements above it
+                # The value of an element is the sum of the two elements
                 value = triangle[row - 1][col - 1] + triangle[row - 1][col]
-                current_row.append(value)        
+                current_row.append(value)
         triangle.append(current_row)
-    
+
     return triangle
